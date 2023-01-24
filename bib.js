5ac1a37421252c06079f2cc503f2ad84e339e9b8
matthiasgeihs <62935430+matthiasgeihs@users.noreply.github.com>
2023-01-24 10:24:44 +0000

    Deploying to gh-pages from @ matthiasgeihs/crypto-graph@418072fd0892340fb4aaa66fa441d95a27cf4cff 🚀

---
+++ bib.js
@@ -0,0 +1,258 @@
+// bibliography
+
+var bibliography = new Map();
+
+bibliography.set('hastad1999pseudorandom', {
+  authors: 'H&#229;stad, Impagliazzo, Levin, and Luby',
+  title: 'A Pseudorandom Generator from Any One-Way Function',
+  journal: "SIAM JoC'99",
+  url: 'http://epubs.siam.org/doi/abs/10.1137/S0097539793244708'
+});
+
+bibliography.set('rudich1984limits', {
+  authors: 'Rudich',
+  title: 'Limits on the Provable Consequences of One-way Functions',
+  journal: "PhD Thesis'84",
+  url: 'http://digitalassets.lib.berkeley.edu/techreports/ucb/text/CSD-88-468.pdf'
+});
+
+bibliography.set('impagliazzo1989limits', {
+  authors: 'Impagliazzo and Rudich',
+  title: 'Limits on the Provable Consequences of One-way Permutations',
+  journal: "STOC'89",
+  url: 'https://dl.acm.org/citation.cfm?id=73012'
+});
+
+bibliography.set('mahmoody2012curious', {
+  authors: 'Mahmoody and Pass',
+  title: 'The Curious Case of Non-Interactive Commitments',
+  journal: "CRYPTO'12",
+  url: 'https://link.springer.com/chapter/10.1007/978-3-642-32009-5_41'
+});
+
+bibliography.set('rompel1990oneway', {
+  authors: 'Rompel',
+  title: 'One-Way Functions are Necessary and Sufficient for Secure Signatures',
+  journal: "STOC'90",
+  url: 'https://dl.acm.org/citation.cfm?id=100269'
+});
+
+bibliography.set('simon1998finding', {
+  authors: 'Simon',
+  title: 'Finding collisions on a one-way street: Can secure hash functions be based on general assumptions?',
+  journal: "EUROCRYPT'98",
+  url: 'https://link.springer.com/chapter/10.1007/BFb0054137'
+});
+
+bibliography.set('yao1982theory', {
+  authors: 'Yao',
+  title: 'Theory and Applications of Trapdoor Functions',
+  journal: "FOCS'82",
+  url: 'http://ieeexplore.ieee.org/document/4568378/'
+});
+
+bibliography.set('naor1989universal', {
+  authors: 'Naor and Yung',
+  title: 'Universal One-Way Hash Functions and their Cryptographic Applications',
+  journal: "STOC'89",
+  url: 'https://dl.acm.org/citation.cfm?id=73011'
+});
+
+bibliography.set('halevi1996practical', {
+  authors: 'Halevi and Micali',
+  title: 'Practical and Provably-Secure Commitment Schemes from Collision-Free Hashing',
+  journal: "CRYPTO'96",
+  url: 'https://link.springer.com/chapter/10.1007/3-540-68697-5_16'
+});
+
+bibliography.set('ishai2005sufficient', {
+  authors: 'Ishai, Kushilevitz, and Ostrovsky',
+  title: 'Sufficient Conditions for Collision-Resistant Hashing',
+  journal: "TCC'05",
+  url: 'https://link.springer.com/chapter/10.1007/978-3-540-30576-7_24'
+});
+
+bibliography.set('naor1991bitcommitment', {
+  authors: 'Naor',
+  title: 'Bit commitment using pseudorandomness',
+  journal: "Journal of Cryptology'91",
+  url: 'https://link.springer.com/article/10.1007%2FBF00196774'
+});
+
+bibliography.set('haitner2007statistically', {
+  authors: 'Haitner and Reingold',
+  title: 'Statistically-hiding commitment from any one-way function',
+  journal: "STOC'07",
+  url: 'https://dl.acm.org/citation.cfm?id=1250792'
+});
+
+bibliography.set('blum1984generate', {
+  authors: 'Blum and Micali',
+  title: 'How to Generate Cryptograpically Strong Sequences of Pseudo-Random Bits',
+  journal: "SIAM JoC'84",
+  url: 'http://epubs.siam.org/doi/abs/10.1137/0213053'
+});
+
+bibliography.set('damgard1987collision', {
+  authors: 'Damg&#229;rd',
+  title: 'Collision free hash functions and public key signature scheme',
+  journal: "EUROCRYPT'87",
+  url: 'https://link.springer.com/chapter/10.1007/3-540-39118-5_19'
+});
+
+bibliography.set('goldwasser1988digital', {
+  authors: 'Goldwasser, Micali, and Rivest',
+  title: 'A digital signature scheme secure against adaptive chosen-message attacks',
+  journal: "SIAM JoC'87",
+  url: 'http://epubs.siam.org/doi/abs/10.1137/0217017'
+});
+
+bibliography.set('bellare1992sign', {
+  authors: 'Bellare and Micali',
+  title: 'How to sign given any trapdoor permutation',
+  journal: "JACM'92",
+  url: 'https://dl.acm.org/citation.cfm?id=147537'
+});
+
+bibliography.set('impagliazzo1989oneway', {
+  authors: 'Impagliazzo and Luby',
+  title: 'One-way functions are essential for complexity based cryptography',
+  journal: "FOCS'89",
+  url: 'http://ieeexplore.ieee.org/document/63483'
+});
+
+bibliography.set('goldreich1986construct', {
+  authors: 'Goldreich, Goldwasser, and Micali',
+  title: 'How to construct random functions',
+  journal: "JACM'86",
+  url: 'https://dl.acm.org/citation.cfm?id=6503'
+});
+
+bibliography.set('goldreich2011collision', {
+  authors: 'Goldreich, Goldwasser, and Micali',
+  title: 'Collision-Free Hashing from Lattice Problems',
+  journal: "Studies in Complexity and Cryptography'11",
+  url: 'https://link.springer.com/chapter/10.1007/978-3-642-22670-0_5'
+});
+
+bibliography.set('luby1988construct', {
+  authors: 'Luby and Rackoff',
+  title: 'How to Construct Pseudo-random Permutations from Pseudo-random Functions',
+  journal: "SIAM JoC'88",
+  url: 'http://epubs.siam.org/doi/abs/10.1137/0217022'
+});
+
+bibliography.set('goldwasser2008lecture', {
+  authors: 'Goldwasser and Bellare',
+  title: 'Lecture Notes on Cryptography',
+  journal: 'Lecture Notes 2008',
+  url: 'https://cseweb.ucsd.edu/~mihir/papers/gb.html'
+});
+
+bibliography.set('ajtai1996generating', {
+  authors: 'Ajtai',
+  title: 'Generating hard instances of lattice problems',
+  journal: "STOC'96",
+  url: 'https://dl.acm.org/citation.cfm?id=237838'
+});
+
+bibliography.set('hemenway2012homomorphic', {
+  authors: 'Hemenway and Ostrovsky',
+  title: 'On Homomorphic Encryption and Chosen-Ciphertext Security',
+  journal: "PKC'12",
+  url: 'https://link.springer.com/chapter/10.1007/978-3-642-30057-8_4'
+});
+
+bibliography.set('crescenzo2000single', {
+  authors: 'Crescenzo, Malkin, and Ostrovsky',
+  title: 'Single Database Private Information Retrieval Implies Oblivious Transfer',
+  journal: "EUROCRYPT'00",
+  url: 'https://link.springer.com/chapter/10.1007/3-540-45539-6_10'
+});
+
+bibliography.set('peikert2008lossy', {
+  authors: 'Peikert and Waters',
+  title: 'Lossy Trapdoor Functions and Their Applications',
+  journal: "STOC'08",
+  url: 'https://dl.acm.org/citation.cfm?id=1374406'
+});
+
+bibliography.set('brakerski2013classical', {
+  authors: 'Brakerski, Langlois, Peikert, Regev, and Stehle',
+  title: 'Classical Hardness of Learning with Errors',
+  journal: "STOC'13",
+  url: 'https://dl.acm.org/citation.cfm?id=2488680'
+});
+
+bibliography.set('rivest1978method', {
+  authors: 'Rivest, Shamir, and Adleman',
+  title: 'A method for obtaining digital signatures and public-key cryptosystems',
+  journal: "Communications of the ACM'78",
+  url: 'https://dl.acm.org/citation.cfm?id=359342'
+});
+
+bibliography.set('unruh2016collapse', {
+  authors: 'Unruh',
+  title: 'Collapse-Binding Quantum Commitments Without Random Oracles',
+  journal: "ASIACRYPT'16",
+  url: 'https://link.springer.com/chapter/10.1007/978-3-662-53890-6_6'
+});
+
+bibliography.set('hohenberger2009short', {
+  authors: 'Hohenberger and Waters',
+  title: 'Short and Stateless Signatures from the RSA Assumption',
+  journal: "CRYPTO'09",
+  url: 'https://link.springer.com/chapter/10.1007/978-3-642-03356-8_38'
+});
+
+bibliography.set('katz2010digital', {
+  authors: 'Katz',
+  title: 'Digital Signatures',
+  journal: "Springer'10",
+  url: 'http://www.springer.com/de/book/9780387277110'
+});
+
+function getReference(refid) {
+  return bibliography.get(refid);
+}
+
+var primitiveDescriptions = new Map();
+
+var MATH_DEFINITIONS = String.raw`
+  \(
+    \def\NN{\mathbb{N}}
+    \def\PTM{\mathsf{PTM}}
+    \def\IPTM{\mathsf{IPTM}}
+    \def\SIG{\mathsf{SIG}}
+    \def\Init{\mathsf{Init}}
+    \def\Proc{\mathsf{Proc}}
+    \def\Fin{\mathsf{Fin}}
+    \def\Return{\mathsf{Return}}
+  \)
+`;
+
+//TODO implement primitive descriptions
+primitiveDescriptions.set('OWF', String.raw`
+  Let \( f:A \to B \) be a function, \( D \) be a probability distribution over \(A\), \(\tau\in\mathbb{N}\), and \(\epsilon \in [0,1]\). We say \(f\) is \( (\tau, \epsilon) \)-one-way for \(D\) if
+  $$ \forall M \in \PTM(\tau): \Pr \begin{bmatrix} x \in  A \land f(x) = y:\\ \begin{bmatrix}f(D) \to y;\\ M(y) \to x;\end{bmatrix} \end{bmatrix} \leq \epsilon \enspace.$$
+
+  Let \(A\) be such that \(\forall \tau \in \NN: |A_{\leq\tau}| < \infty\), where \(A_{\leq\tau} = \{x \in A: |x| \leq \tau\}\).
+  We say \( f:A \to B \) is \( (\tau, \epsilon) \)-one-way if \(f\) is \( (\tau, \epsilon) \)-one-way for \( U_{A_{\leq \tau}} \), where \( U_{A_{\leq \tau}} \) is the uniform distribution over \(A_{\leq\tau}\).
+
+  <br><br>
+
+    Let \(\epsilon:\NN \to [0,1]\). We say \(f\) is \(\epsilon\)-one-way for \(D\) if for all \(\tau \in \NN\), \(f\) is \((\tau, \epsilon(\tau))\)-one-way for \(D\).
+
+  <br><br>
+
+    Let \(A\) be such that \(\forall \tau \in \NN: |A_{\leq\tau}| < \infty\).
+    Let \(\epsilon:\NN \to [0,1]\). We say \(f\) is \(\epsilon\)-one-way if for all \(\tau \in \NN\), \(f\) is \((\tau, \epsilon(\tau))\)-one-way for \( U_{A_{\leq \tau}} \).
+
+  <br><br>
+
+  We say \( f:A \to B \) is one-way if there exists a negligible function \(\epsilon\) such that for all \(\tau \in \NN\), \(f\) is \((\tau,\epsilon(\tau))\)-one-way.
+  `);
+
+function getPrimitiveDescription(primid) {
+  return primitiveDescriptions.get(primid);
+}
